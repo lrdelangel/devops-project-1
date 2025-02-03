c5196851667158ea5eaa78c7f453680c6e04f4a3
lrdelangel <luis.delangel@luis-delangel.local>
2025-02-03 17:26:36 -0600

    changes

--- infra/remote_backend_s3.tf
+++ infra/remote_backend_s3.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "dev-proj-1-remote-state-bucket"
+    bucket = "dev-proj-1-remote-state-bucket-2"
     key    = "devops-project-1/terraform.tfstate"
     region = "us-east-1"
   }
