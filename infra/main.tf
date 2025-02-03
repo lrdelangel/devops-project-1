7fdd49d3ab397e094b7ad5bf7c4fba532899d873
lrdelangel <luis.delangel@luis-delangel.local>
2025-02-03 16:53:55 -0600

    cambios

--- infra/main.tf
+++ infra/main.tf
@@ -1,4 +1,4 @@
-/*module "s3" {
+/*module "s3" { 
   source      = "./s3"
   bucket_name = var.bucket_name
   name        = var.name
@@ -10,7 +10,7 @@ module "networking" {
   vpc_cidr             = var.vpc_cidr
   vpc_name             = var.vpc_name
   cidr_public_subnet   = var.cidr_public_subnet
-  eu_availability_zone = var.eu_availability_zone
+  us_availability_zone = var.us_availability_zone
   cidr_private_subnet  = var.cidr_private_subnet
 }
 
